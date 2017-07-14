3477fc8554725d8e6ccb40044bcd01112033301a
lijianqiang <silenceljq@gmail.com>
2017-07-14 23:14:42 +0800

    Update start.sh

--- spring-boot-demo/bash/start.sh
+++ spring-boot-demo/bash/start.sh
@@ -10,7 +10,7 @@ echo "conf: "$APP_CONF
 echo "log: "$APP_LOG
 echo ""
 
-JAVA_OPTS='-server -Xrs -Xmx2g -Xms2g -Xmn512m -XX:PermSize=256m -Xss256k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullC
+JAVA_OPTS='-server -Xrs -Xmx1024m -Xms1024m -Xmn512m -XX:PermSize=256m -Xss256k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullC
 ollection -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -XX:SurvivorRatio=8 -Xverify:none -
 Djava.net.preferIPv4Stack=true -Djava.security.egd=file:/dev/./urandom -Dfile.encoding=UTF8 -Dlog4j.log.port=8001'
 
