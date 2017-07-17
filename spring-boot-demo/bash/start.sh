2727872a4f9d07fc52bad0b177c61bb69ff40fda
lijianqiang <silenceljq@gmail.com>
2017-07-17 20:06:22 +0800

    Update start.sh

--- spring-boot-demo/bash/start.sh
+++ spring-boot-demo/bash/start.sh
@@ -10,9 +10,7 @@ echo "conf: "$APP_CONF
 echo "log: "$APP_LOG
 echo ""
 
-JAVA_OPTS='-server -Xrs -Xmx1024m -Xms1024m -Xmn512m -XX:PermSize=256m -Xss256k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullC
-ollection -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -XX:SurvivorRatio=8 -Xverify:none -
-Djava.net.preferIPv4Stack=true -Djava.security.egd=file:/dev/./urandom -Dfile.encoding=UTF8 -Dlog4j.log.port=8001'
+JAVA_OPTS='-server -Xrs -Xmx1024m -Xms1024m -Xmn512m -XX:PermSize=256m -Xss256k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSCompactAtFullCollection -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -XX:SurvivorRatio=8 -Xverify:none -Djava.net.preferIPv4Stack=true -Djava.security.egd=file:/dev/./urandom -Dfile.encoding=UTF8 -Dlog4j.log.port=8001'
 
 
 nohup java $JVM_OPTS -jar $APP_DIR/$APP_NAME --spring.config.location=$APP_CONF > $APP_LOG &
